63b24a530fafdeae6951e07e3e60737fc6966077
shahrojali001 <108857096+shahrojali001@users.noreply.github.com>
2022-07-07 21:40:19 +0530

    Create Number_pyramid.py

---
+++ Work/Day1/Number_pyramid.py
@@ -0,0 +1,57 @@
+"""Defination:to find the number pattern """
+def patt(row):
+    """algorithm of number pattern"""
+    digit_range=1
+    digit=1
+    while digit<=row:
+        gap=1
+        while gap<=row-digit:
+            print(" ",end=" ")
+            gap=gap+1
+        j=1
+        while j<=digit_range:
+            print(digit,end=" ")
+            j=j+1
+        digit_range=digit_range+2
+        print()
+        digit=digit+1
+if __name__=='__main__':
+    RAW=int(input("enter the number of rows: "))
+    patt(RAW)
+
+
+
+    
+
+    
+
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
